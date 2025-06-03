9ccdf5e020db05a0e2029669c3e6316b2a7e6529
Sabolch <sabolch.dev@gmail.com>
2025-06-03 22:26:39 +0200

    Commit

--- database.js
+++ database.js
@@ -1,11 +1,17 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD, 
+  host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  }
   
 });
 
